6e91fa7d51e2f286234b8ade1853119e6cb45f58
leonardo <leowalkg6@Gmail.com>
2025-07-27 18:26:39 -0300

    criando produtos

--- app/crud/produto.py
+++ app/crud/produto.py
@@ -10,3 +10,16 @@ def get_produtos(db:Session):
 def get_produto(db: Session,id:str):
     return db.query(Produto).filter(Produto.id == id).first()
     # retorna um produto específico pelo ID
+
+def create_produto(db: Session, produto: ProdutoCreate):
+    db_produto = Produto(
+        id=produto.id,
+        nome=produto.nome,
+        preco=produto.preco,
+        quantidade=produto.quantidade
+    )
+    db.add(db_produto)
+    db.commit()
+    db.refresh(db_produto)
+    return db_produto
+    # cria um novo produto no banco de dados
