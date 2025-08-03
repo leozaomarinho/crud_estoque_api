124d2a7c6f5d61b3219e3ddbeca497ac4d88baa3
leonardo <leowalkg6@Gmail.com>
2025-08-02 23:00:47 -0300

    adicionando services

--- app/services/produto_service.py
+++ app/services/produto_service.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
 from app import schemas, models
+from app.schemas import ProdutoCreate, ProdutoUpdate
 
 class ProdutoService:
 
@@ -22,3 +23,24 @@ class ProdutoService:
     @staticmethod
     def obter_produto(db: Session, produto_id: int):
         return db.query(models.Produto).filter(models.Produto.id == produto_id).first()
+    
+    @staticmethod
+
+    def atualizar_produto(db: Session, produto_id: int, produto: schemas.ProdutoCreate):
+        db_produto = db.query(models.Produto).filter(models.Produto.id == produto_id).first()
+        if not db_produto:
+            return None
+        for key, value in produto.dict(exclude_unset=True).items():
+            setattr(db_produto, key, value)
+        db.commit()
+        db.refresh(db_produto)
+        return db_produto
+    
+    def excluir_produto(db: Session, produto_id: int):
+        db_produto = db.query(models.Produto).filter(models.Produto.id == produto_id).first()
+        if not db_produto:
+            return None
+        db.delete(db_produto)
+        db.commit()
+        return db_produto
+    
